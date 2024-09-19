ea328b94bcfe4fccc3c4d4e903a9469b67e541fc
Gifftybabe <ulokangozi@gmail.com>
2024-09-19 15:13:36 +0100

    Create function

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -31,4 +31,37 @@ contract SwapToken {
       mapping(uint => OrderSwap) OrderIdToOrders; //Order => Order
 
     constructor() {}
+
+
+ function createOrder(
+        uint _depositedAmount,
+        address _depositorToken,
+        uint _requestedAmount,
+        address _requestedToken
+    ) external {
+        require(msg.sender != address(0), "Address zero detected.");
+        require(_depositorToken != address(0), "Address zero detected.");
+        require(_requestedToken != address(0), "Address zero detected.");
+        require(_depositedAmount > 0, "Invalid Deposit Amount");
+        require(_requestedAmount > 0, "Invalid Requested Amount");
+
+        require(
+            IERC20(_depositorToken).transferFrom(
+                msg.sender,
+                address(this),
+                _depositedAmount
+            ),
+            "Transfer Failed"
+        );
+
+        createTransaction(
+            _depositedAmount,
+            _depositorToken,
+            _requestedAmount,
+            _requestedToken,
+            Order.OrderCreated,
+            OrderStatus.Open
+        );
+    
+    }
 }
